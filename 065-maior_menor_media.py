f5496cb39c82b0bc9558ecb4e4bf730c510093a3
Vinícius Di França Alarcão Oliveira <102164900+viniciusAlarc@users.noreply.github.com>
2024-09-04 00:59:23 -0300

    Update 065-maior_menor_media.py

--- 065-maior_menor_media.py
+++ 065-maior_menor_media.py
@@ -1,5 +1,5 @@
 '''
-Crie um programa que leia vário números no teclado. No final da execução, mostre a média entre todos os valores e qual foi
+Crie um programa que leia vários números no teclado. No final da execução, mostre a média entre todos os valores e qual foi
 o maior e o menor valores lidos. O programa deveperguntar ao usuário se ele que ou não continuar a digitar valores.
 '''
 cont = 0
